1dca9ae2d79ded3aa2130518586eb569d14d3241
Michael Humelsine <mhumelsine@isf.com>
2026-09-25 13:14:56 -0400

    Adding manual cleanup for OOSC with remaining bac dockets

---
+++ Cleanups/06_OOSC_WithRemaining_BadDockets/Cleanup_06_OOSC_WithRemaining_BadDockets.cs
@@ -0,0 +1,26 @@
+using JisCleanup.TableChanges;
+
+namespace JisCleanup.Cleanups;
+
+[ManualCleanup("data.csv")]
+public class Cleanup_06_OOSC_WithRemaining_BadDockets : CleanupBase {
+    public Cleanup_06_OOSC_WithRemaining_BadDockets()
+    {
+        
+        Metadata = new CleanupMetadata
+        {
+            Name = GetType().Name,
+            Description = "Case that had a OOSC after the bad docket entries, but the bad docket entries were not removed.",
+            RequestedBy = "JIS"
+        };
+        
+        Validations =
+        [  
+        ];
+        
+        Changes = [
+            new CjisDocketDelete(),
+            new InsertCleanupDocketEntry()
+        ];
+    }
+}
